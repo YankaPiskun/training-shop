98d193bf84671885cbd15f31eb19e8217fb7d9b9
YankaPiskun <yan.piskun.2000@bk.ru>
2022-02-07 16:27:41 +0300

    set router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router,} from "react-router-dom";
 import AppRouter from "./components/AppRouter";
 import Navbar from "./components/UI/Navbar/Navbar";
 import Topbar from "./components/UI/Navbar/Topbar";
--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Main from "../pages/Main";
 import Post from "../pages/Post";
 
 const AppRouter = () => {
   return (
     <Routes>
-      <Route path="post/main" element={<Main></Main>}></Route>
-      <Route path="/post" element={<Post></Post>}></Route>
-      <Route path="*" element={<Navigate to="/post" replace />} />
+      <Route path="/training-shop" element={<Main></Main>}></Route>
+      <Route path="training-shop/post" element={<Post></Post>}></Route>
+      <Route path="*" element={<Navigate to="/training-shop" replace />} />
     </Routes>
   );
 };
